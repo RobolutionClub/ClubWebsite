092f5822eacaeeaab04f98781f58f9e443fa86b3
AnushkaThakur <76484422+AnushkaThakur@users.noreply.github.com>
2022-06-11 21:52:05 +0530

    Add files via upload

---
+++ events.js
@@ -0,0 +1,64 @@
+<div id="st-container" class="st-container disable-scrolling"> 
+<div class="st-pusher"> 
+<div class="st-content"> 
+<!-- Begin Top Section --> 
+<section id="top-section" class="top-section image-section " style=""> 
+<div class="overlay gradient-overlay">
+</div>
+ <header id="top-header" class="top-header"> 
+ <div class="overlay white-solid">
+ </div> 
+ 
+ 
+  <div class="content-wrapper">
+  <div class="jumbotron text-left">
+  <div class="animated fadeInLeft visible" data-animation="fadeInLeft" data-delay="500">
+  <h1>Events</h1>
+  </div> 
+  </div>
+  </div> 
+  </section> 
+  <!-- End Top Section -->
+  <!-- Begin Events List-->
+  <section id="speakers">
+  <div class="content-wrapper">
+  <div class="col-lg-10 col-lg-offset-1 appear-animation-trigger"> 
+  <div class="effect-wrapper col-md-4 col-sm-6 col-xs-12 cols-centered appear-animation visible" style="transition-delay: 0.08037s; transition-duration: 0.2s;">
+  <div class="zoe-effect ribbon-activator"> 
+  <figure class="waves-effect waves-block waves-light" style="background-image: url(https://www.pexels.com/search/robot/)">
+  <a class="overlay solid-overlay" href="..."></a> 
+  <ul class="ribbon-wrapper">
+  <li> <p class="ribbon abbr">...</p>
+  <p class="ribbon full-title">pe</p>
+  </li>
+  </ul>
+  <figcaption>
+  <div class="col-md-8 col-xs-8 text-left">
+  <h2 class="name">
+  <a href="..">past events</a></h2> 
+  <div class="clearfix"></div>
+  <span class="position"> events to be remembered</span> 
+  </div>
+  </figcaption> 
+  </figure>
+  </div>
+  </div>
+  <div class="effect-wrapper col-md-4 col-sm-6 col-xs-12 cols-centered appear-animation visible" style="transition-delay: 0.22372s; transition-duration: 0.2s;">
+  <div class="zoe-effect ribbon-activator"> 
+  <figure class="waves-effect waves-block waves-light" style="background-image: url(https://www.pexels.com/search/robot/)">
+  <a class="overlay solid-overlay" href="...."></a>
+  <ul class="ribbon-wrapper"> <li> 
+  <p class="ribbon abbr">...</p>
+  <p class="ribbon full-title"> ue</p> </li> </ul> 
+  <figcaption>
+  <div class="col-md-8 col-xs-8 text-left">
+  <h2 class="name">
+  <a href="https://2020.robotix.in/event/nexus/">upcoming events</a></h2>
+  <div class="clearfix"></div> <span class="position">events to come</span>
+  </div>
+  </figcaption>
+  </figure> 
+  </div>
+  </div>
+ 
+  <!-- End Events List -->
